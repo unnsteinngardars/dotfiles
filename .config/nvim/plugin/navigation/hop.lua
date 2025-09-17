336cc4ca7ed06b07fc6b7718c53d5c33746ad05c
Unnsteinn Garðarsson <unnsteinn.gardarsson@cert.is>
2025-09-17 10:57:59 +0000

    LazyVim

--- .config/nvim/plugin/navigation/hop.lua
+++
@@ -1,9 +0,0 @@
-local wk = require("which-key")
-
-wk.add({
-    {"fw", ":HopWord <CR>", desc = "Hop word", mode = "n"},
-    {"fp", ":HopPattern <CR>", desc = "Hop pattern", mode = "n"},
-    {"ff", ":HopChar1 <CR>", desc = "Hop char 1", mode = "n"},
-    {"fg", ":HopChar2 <CR>", desc = "Hop char 2", mode = "n"},
-    {"fl", ":HopLine <CR>", desc = "Hop line", mode = "n"},
-})
--- .config/nvim/plugin/navigation/nvim-window.lua
+++
@@ -1,6 +0,0 @@
-local wk = require("which-key")
-
-wk.add({
-    {"<leader>w", ":lua require('nvim-window').pick()<CR>", desc = "Pick window", mode = "n"},
-})
-
